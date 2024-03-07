6c7ebb698b036a9b22a9241cb03d6662100619a6
Piotr Karamon <Piotr Karamon>
2024-03-07 12:25:02 +0100

    changes during lecture

--- cw01/countdown.c
+++ cw01/countdown.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main()
+int main(int argc, char* args[])
 {
+    if(argc > 1) {
+        printf("arg: %s\n", args[1]);
+    }
+
     for (int i = 10; i >= 0; i--) {
         printf("%d\n", i);
     }
