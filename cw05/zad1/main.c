7bc82cc780b277c6eab4d1ac5e792c03751f8680
Piotr Karamon <piotrkaramon3@gmail.com>
2024-04-09 21:16:21 +0200

    feat: finished cw05/zad1

--- cw05/zad1/main.c
+++ cw05/zad1/main.c
@@ -15,21 +15,48 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
-  char *reaction_type = argv[1];
+  char *react_t = argv[1];
+  struct sigaction act;
+  sigemptyset(&act.sa_mask);
+  act.sa_flags = 0;
 
-  if (strcmp(reaction_type, "none") == 0) {
-    signal(SIGUSR1, SIG_DFL);
-  } else if (strcmp(reaction_type, "ignore") == 0) {
-    signal(SIGUSR1, SIG_IGN);
-  } else if (strcmp(reaction_type, "handler") == 0) {
-    signal(SIGUSR1, handler);
-  } else if (strcmp(reaction_type, "mask") == 0) {
+  if (strcmp(react_t, "none") == 0) {
+    act.sa_handler = SIG_DFL;
+  } else if (strcmp(react_t, "ignore") == 0) {
+    act.sa_handler = SIG_IGN;
+  } else if (strcmp(react_t, "handler") == 0) {
+    act.sa_handler = handler;
+  } else if (strcmp(react_t, "mask") == 0) {
+    sigset_t mask;
+    sigaddset(&mask, SIGUSR1);
+    if (sigprocmask(SIG_SETMASK, &mask, NULL) == -1) {
+      perror("cuold not setup mask");
+      return EXIT_FAILURE;
+    }
   } else {
-    fprintf(stderr, "unknown reaction type: %s\n", reaction_type);
+    fprintf(stderr, "unknown reaction type: %s\n", react_t);
     return EXIT_FAILURE;
   }
 
-  raise(SIGUSR1);
+  if (strcmp(react_t, "mask") != 0 && sigaction(SIGUSR1, &act, NULL) == -1) {
+    perror("could not setup signal handling");
+    return EXIT_FAILURE;
+  }
+
+  if (raise(SIGUSR1) != 0) {
+    fprintf(stderr, "could not raise signal\n");
+    return EXIT_FAILURE;
+  }
+
+  if (strcmp(react_t, "mask") == 0) {
+    sigset_t pending_signals;
+    if (sigpending(&pending_signals) == -1) {
+      perror("could not get pending signals");
+      return EXIT_FAILURE;
+    }
+    printf("SIGUSR1 is pending: %s\n",
+           sigismember(&pending_signals, SIGUSR1) ? "yes" : "no");
+  }
 
   return EXIT_SUCCESS;
 }
